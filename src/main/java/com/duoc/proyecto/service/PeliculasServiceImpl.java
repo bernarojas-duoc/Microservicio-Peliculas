4bcb4689761bc6a59f41fc0bee8f38c14a1e8129
@frontend_xenda <242213256+bbernarojas@users.noreply.github.com>
2026-04-06 04:23:45 -0400

    feat: Update PeliculasServiceImpl and application properties for improved configuration

--- src/main/java/com/duoc/proyecto/service/PeliculasServiceImpl.java
+++ src/main/java/com/duoc/proyecto/service/PeliculasServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.duoc.proyecto.model.Pelicula;
 import com.duoc.proyecto.repository.PeliculaRepository;
 
+@Service
 public class PeliculasServiceImpl implements PeliculaService {
   @Autowired
   private PeliculaRepository peliculaRepository;
