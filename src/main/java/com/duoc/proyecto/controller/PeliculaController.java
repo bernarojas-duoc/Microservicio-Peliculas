9571f105f8439b3238fc873e5d34ad72b6151405
@frontend_xenda <242213256+bbernarojas@users.noreply.github.com>
2026-04-09 21:54:46 -0400

    feat: Add modificarPelicula and eliminarPelicula endpoints to PeliculaController

--- src/main/java/com/duoc/proyecto/controller/PeliculaController.java
+++ src/main/java/com/duoc/proyecto/controller/PeliculaController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.duoc.proyecto.model.Pelicula;
 import com.duoc.proyecto.service.PeliculaService;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 
@@ -41,5 +43,16 @@ public class PeliculaController {
   public Pelicula crearPelicula(@Valid @RequestBody Pelicula pelicula) {
     return peliculaService.createPelicula(pelicula);
   }
-  
+
+  // Modificar película
+  @PutMapping("/{id}")
+  public Pelicula modificarPelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+    return peliculaService.updatePelicula(id, pelicula);
+  }
+
+  // Eliminar película
+  @DeleteMapping("/{id}")
+  public void eliminarPelicula(@PathVariable Long id) {
+    peliculaService.deletePelicula(id);
+  }
 }
